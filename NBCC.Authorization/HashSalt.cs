1500a7beb85df8f22f32a42b55d2c4f944ba31b8
GREG <GREG.BOUDREAU@GMAIL.COM>
2023-04-08 13:31:51 -0300

    TEST

---
+++ NBCC.Authorization/HashSalt.cs
@@ -0,0 +1,12 @@
+﻿namespace NBCC.Authorization;
+
+public class HashSalt
+{
+    public byte[] Hash { get; private set; }
+    public byte[] Salt { get; private set; }
+    public HashSalt(byte[] hash, byte[] salt)
+    {
+        Hash = hash;
+        Salt = salt;
+    }
+}
