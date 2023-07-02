8e97235d8a6837be3d997e6bd1be1807b55e2ab7
gregb <gregb@LAPPY>
2023-07-02 11:34:54 -0300

    fixing logic for archived course fetch

--- NBCC.Instructors/DataAccess/Queries/SELECT_InstructorsByCourse.sql
+++ NBCC.Instructors/DataAccess/Queries/SELECT_InstructorsByCourse.sql
@@ -3,7 +3,7 @@ SELECT i.InstructorId
 	  ,i.LastName
 	  ,i.Email
   FROM [College].[Assignments] c
-  INNER JOIN College.Courses CO ON C.CourseId = CO.CourseId AND CO.IsArchived=0
+  INNER JOIN College.Courses CO ON C.CourseId = CO.CourseId 
   INNER JOIN Staff.Instructors i on i.InstructorId = c.InstructorId and i.IsArchived=0
   WHERE co.courseid = @CourseId
   AND c.IsArchived = 0
--- NBCC.Instructors/Properties/SqlScript.Designer.cs
+++ NBCC.Instructors/Properties/SqlScript.Designer.cs
@@ -182,7 +182,7 @@ namespace NBCC.Instructors.Properties {
         ///	  ,i.LastName
         ///	  ,i.Email
         ///  FROM [College].[Assignments] c
-        ///  INNER JOIN College.Courses CO ON C.CourseId = CO.CourseId AND CO.IsArchived=0
+        ///  INNER JOIN College.Courses CO ON C.CourseId = CO.CourseId 
         ///  INNER JOIN Staff.Instructors i on i.InstructorId = c.InstructorId and i.IsArchived=0
         ///  WHERE co.courseid = @CourseId
         ///  AND c.IsArchived = 0.
